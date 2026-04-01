a0e5eb23aca71c078077f62289074025c5b4ed22
whyuascii <eduardo.eddy.verde94@gmail.com>
2026-04-01 01:54:47 -0400

    refactor: remove unused helper files and ci-snippets view

--- backend/integrations/views/helpers.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import get_object_or_404
-
-from projects.models import Project
-
-
-def get_project_for_user(request, project_slug):
-    return get_object_or_404(Project, slug=project_slug, owner=request.user)
